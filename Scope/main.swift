c04e953bd0d49a7fab7bd4dedc9e470ce45eb5ad
David Matterne <david.matterne@gmail.com>
2016-09-27 13:44:17 +0200

    Initial Commit

---
+++ Scope/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Scope
+//
+//  Created by Stannis Baratheon on 27/09/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
